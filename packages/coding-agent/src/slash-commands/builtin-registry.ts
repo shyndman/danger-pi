Raudbjorn/omp

feat: live reload native commands and skills
Watch native .omp command and skill roots so runtime slash-command and skill state can refresh without restarting the app. The explicit /reload recovery path, move handling, and settings-side effects all stay wired together so watcher failures remain visible and runtime rediscovery stays coherent.

packages/coding-agent/src/slash-commands/builtin-registry.ts
@@ -562,6 +562,16 @@ const CORE_BUILTIN_SLASH_COMMAND_REGISTRY: ReadonlyArray<BuiltinSlashCommandSpec
 			await runtime.ctx.handleMemoryCommand(command.text);
 		},
 	},
+	// Fork integration for hot reloading: explicit recovery command for watcher/runtime refresh.
+	{
+		name: "reload",
+		description: "Retry OMP live reload and refresh runtime command state",
+		allowBatch: true,
+		handle: async (_command, runtime) => {
+			runtime.ctx.editor.setText("");
+			await runtime.ctx.handleReloadCommand();
+		},
+	},
 	{
 		name: "rename",
 		description: "Rename the current session",

shyndman/danger-pi

packages/coding-agent/src/slash-commands/builtin-registry.ts
@@ -964,6 +964,16 @@ const CORE_BUILTIN_SLASH_COMMAND_REGISTRY: ReadonlyArray<SlashCommandSpec> = [
 			await runtime.ctx.handleMemoryCommand(command.text);
 		},
 	},
+	// Fork integration for hot reloading: explicit recovery command for watcher/runtime refresh.
+	{
+		name: "reload",
+		description: "Retry OMP live reload and refresh runtime command state",
+		allowBatch: true,
+		handleTui: async (_command, runtime) => {
+			runtime.ctx.editor.setText("");
+			await runtime.ctx.handleReloadCommand();
+		},
+	},
 	{
 		name: "rename",
 		description: "Rename the current session",