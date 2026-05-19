Raudbjorn/omp

feat: preserve multi-block submissions
Let stacked slash commands, shortcut blocks, and trailing text submit in author order without collapsing them into a single raw prompt. The session now records intermediate multi-block transcript entries, skips builtin command echo in model context, and can continue from already-persisted context when the final renderable block is a command.

fix(coding-agent): restore multi-block transcript behavior

Multi-block submissions now preserve the user-visible transcript contract: supported skill and text blocks render in source order, /btw can start a follow-up turn without leaving a transcript artifact, and unsupported builtins warn and skip instead of recording surrogate command messages.

The fix moves policy into fork-owned multi-block helpers, keeps ACP replay flattening aligned with main, and makes continue-from-context submissions explicit so the live session and rebuilt history tell the same story.

packages/coding-agent/src/modes/controllers/shortcut-command-executor.ts
@@ -1,0 +1,52 @@
+import type { InteractiveModeContext } from "../types";
+
+/**
+ * Fork-local helper that centralizes shortcut execution behavior so we can iterate on multi-block
+ * shortcut experiments without expanding upstream-owned `input-controller` logic.
+ */
+
+export interface ShortcutExecutionOptions {
+	historyEntry?: string;
+}
+
+export async function executeBashShortcut(
+	ctx: InteractiveModeContext,
+	command: string,
+	excludeFromContext: boolean,
+	options?: ShortcutExecutionOptions,
+): Promise<boolean> {
+	if (ctx.session.isBashRunning) {
+		ctx.showWarning("A bash command is already running. Press Esc to cancel it first.");
+		return false;
+	}
+
+	if (options?.historyEntry) {
+		ctx.editor.addToHistory(options.historyEntry);
+	}
+
+	await ctx.handleBashCommand(command, excludeFromContext);
+	ctx.isBashMode = false;
+	ctx.updateEditorBorderColor();
+	return true;
+}
+
+export async function executePythonShortcut(
+	ctx: InteractiveModeContext,
+	code: string,
+	excludeFromContext: boolean,
+	options?: ShortcutExecutionOptions,
+): Promise<boolean> {
+	if (ctx.session.isPythonRunning) {
+		ctx.showWarning("A Python execution is already running. Press Esc to cancel it first.");
+		return false;
+	}
+
+	if (options?.historyEntry) {
+		ctx.editor.addToHistory(options.historyEntry);
+	}
+
+	await ctx.handlePythonCommand(code, excludeFromContext);
+	ctx.isPythonMode = false;
+	ctx.updateEditorBorderColor();
+	return true;
+}

shyndman/danger-pi

packages/coding-agent/src/modes/controllers/shortcut-command-executor.ts
@@ -1,0 +1,52 @@
+import type { InteractiveModeContext } from "../types";
+
+/**
+ * Fork-local helper that centralizes shortcut execution behavior so we can iterate on multi-block
+ * shortcut experiments without expanding upstream-owned `input-controller` logic.
+ */
+
+export interface ShortcutExecutionOptions {
+	historyEntry?: string;
+}
+
+export async function executeBashShortcut(
+	ctx: InteractiveModeContext,
+	command: string,
+	excludeFromContext: boolean,
+	options?: ShortcutExecutionOptions,
+): Promise<boolean> {
+	if (ctx.session.isBashRunning) {
+		ctx.showWarning("A bash command is already running. Press Esc to cancel it first.");
+		return false;
+	}
+
+	if (options?.historyEntry) {
+		ctx.editor.addToHistory(options.historyEntry);
+	}
+
+	await ctx.handleBashCommand(command, excludeFromContext);
+	ctx.isBashMode = false;
+	ctx.updateEditorBorderColor();
+	return true;
+}
+
+export async function executePythonShortcut(
+	ctx: InteractiveModeContext,
+	code: string,
+	excludeFromContext: boolean,
+	options?: ShortcutExecutionOptions,
+): Promise<boolean> {
+	if (ctx.session.isEvalRunning) {
+		ctx.showWarning("A Python execution is already running. Press Esc to cancel it first.");
+		return false;
+	}
+
+	if (options?.historyEntry) {
+		ctx.editor.addToHistory(options.historyEntry);
+	}
+
+	await ctx.handlePythonCommand(code, excludeFromContext);
+	ctx.isPythonMode = false;
+	ctx.updateEditorBorderColor();
+	return true;
+}