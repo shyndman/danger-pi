Raudbjorn/omp

feat: preserve multi-block submissions
Let stacked slash commands, shortcut blocks, and trailing text submit in author order without collapsing them into a single raw prompt. The session now records intermediate multi-block transcript entries, skips builtin command echo in model context, and can continue from already-persisted context when the final renderable block is a command.

fix(coding-agent): restore multi-block transcript behavior

Multi-block submissions now preserve the user-visible transcript contract: supported skill and text blocks render in source order, /btw can start a follow-up turn without leaving a transcript artifact, and unsupported builtins warn and skip instead of recording surrogate command messages.

The fix moves policy into fork-owned multi-block helpers, keeps ACP replay flattening aligned with main, and makes continue-from-context submissions explicit so the live session and rebuilt history tell the same story.

packages/coding-agent/src/modes/components/tree-selector.ts
@@ -13,9 +13,10 @@ import {
 import type { TreeFilterMode } from "../../config/settings-schema";
 import { theme } from "../../modes/theme/theme";
 import { matchesAppInterrupt } from "../../modes/utils/keybinding-matchers";
-import type { SessionTreeNode } from "../../session/session-manager";
+import type { CustomMessageEntry, SessionTreeNode } from "../../session/session-manager";
 import { shortenPath } from "../../tools/render-utils";
 import { DynamicBorder } from "./dynamic-border";
+import { formatCustomMessageSummary, getCustomMessageLabel } from "./message-labels";
 
 /** Gutter info: position (displayIndent where connector was) and whether to show │ */
 interface GutterInfo {
@@ -370,12 +371,9 @@ class TreeList implements Component {
 				break;
 			}
 			case "custom_message": {
-				parts.push(entry.customType);
-				if (typeof entry.content === "string") {
-					parts.push(entry.content);
-				} else {
-					parts.push(this.#extractContent(entry.content));
-				}
+				const customEntry = entry as CustomMessageEntry;
+				parts.push(getCustomMessageLabel(customEntry) ?? customEntry.customType);
+				parts.push(formatCustomMessageSummary(customEntry));
 				break;
 			}
 			case "compaction":
@@ -569,14 +567,7 @@ class TreeList implements Component {
 				break;
 			}
 			case "custom_message": {
-				const content =
-					typeof entry.content === "string"
-						? entry.content
-						: entry.content
-								.filter((c): c is { type: "text"; text: string } => c.type === "text")
-								.map(c => c.text)
-								.join("");
-				result = theme.fg("customMessageLabel", `[${entry.customType}]: `) + normalize(content);
+				result = theme.fg("customMessageLabel", formatCustomMessageSummary(entry as CustomMessageEntry));
 				break;
 			}
 			case "compaction": {

shyndman/danger-pi

packages/coding-agent/src/modes/components/tree-selector.ts
@@ -14,12 +14,13 @@ import {
 import type { TreeFilterMode } from "../../config/settings-schema";
 import { theme } from "../../modes/theme/theme";
 import { matchesAppInterrupt, matchesSelectDown, matchesSelectUp } from "../../modes/utils/keybinding-matchers";
-import type { SessionTreeNode } from "../../session/session-entries";
+import type { CustomMessageEntry, SessionTreeNode } from "../../session/session-entries";
 import { toPathList } from "../../tools/path-utils";
 import { shortenPath } from "../../tools/render-utils";
 import { canonicalizeMessage } from "../../utils/thinking-display";
 import { resolveAssistantErrorPresentation } from "../utils/transcript-render-helpers";
 import { DynamicBorder } from "./dynamic-border";
+import { formatCustomMessageSummary, getCustomMessageLabel } from "./message-labels";
 import { centeredWindow, contentRowWidth, renderScrollableList } from "./selector-helpers";
 
 /** Gutter info: position (displayIndent where connector was) and whether to show │ */
@@ -375,12 +376,9 @@ class TreeList implements Component {
 				break;
 			}
 			case "custom_message": {
-				parts.push(entry.customType);
-				if (typeof entry.content === "string") {
-					parts.push(entry.content);
-				} else {
-					parts.push(this.#extractContent(entry.content));
-				}
+				const customEntry = entry as CustomMessageEntry;
+				parts.push(getCustomMessageLabel(customEntry) ?? customEntry.customType);
+				parts.push(formatCustomMessageSummary(customEntry));
 				break;
 			}
 			case "compaction":
@@ -650,14 +648,7 @@ class TreeList implements Component {
 				break;
 			}
 			case "custom_message": {
-				const content =
-					typeof entry.content === "string"
-						? entry.content
-						: entry.content
-								.filter((c): c is { type: "text"; text: string } => c.type === "text")
-								.map(c => c.text)
-								.join("");
-				result = theme.fg("customMessageLabel", `[${entry.customType}]: `) + normalize(content);
+				result = theme.fg("customMessageLabel", formatCustomMessageSummary(entry as CustomMessageEntry));
 				break;
 			}
 			case "compaction": {