Raudbjorn/omp

feat: preserve sticky Codex selection and welcome styling
Keep the fork's sticky Codex account selection behavior so a chosen Codex identity remains stable within a session and falls back intentionally when usage data blocks it. Also preserve the default pruning tweak and the fork-local welcome screen styling that the replay branch should continue to present.

packages/coding-agent/src/modes/components/welcome.ts
@@ -2,6 +2,9 @@ import { type Component, padding, truncateToWidth, visibleWidth } from "@oh-my-p
 import { APP_NAME } from "@oh-my-pi/pi-utils";
 import { theme } from "../../modes/theme/theme";
 
+const WELCOME_BORDER_HOT_PINK_ANSI = "\x1b[38;2;247;18;232m";
+const ANSI_FOREGROUND_RESET = "\x1b[39m";
+
 export interface RecentSession {
 	name: string;
 	timeAgo: string;
@@ -69,10 +72,22 @@ export class WelcomeComponent implements Component {
 
 		// Block-based OMP logo (gradient: magenta → cyan)
 		// biome-ignore format: preserve ASCII art layout
-		const piLogo = ["▀████████████▀", " ╘███    ███  ", "  ███    ███  ", "  ███    ███  ", " ▄███▄  ▄███▄ "];
+		const piLogo = [
+			"     #▓░▓##       ",
+			"   #░░░░░░░▓#     ",
+			"  ##░░░░░░░▓░#    ",
+			"  #█▒██\ue22c ██▓█#    ",
+			"  #█ X ██ X █#    ",
+			"  #█░░█  ░░██#    ",
+			"░░  (██░███)  /░  ",
+			"░▒*._█ █  █  /░░/ ",
+			"    \\#// /       ",
+			"░░░▒** - - *░░░░\\ ",
+			"░░           ░▓  ",
+		];
 
 		// Apply gradient to logo
-		const logoColored = piLogo.map(line => this.#gradientLine(line));
+		const logoColored = this.#radialGradient(piLogo, 8, 4);
 
 		// Left column - centered content
 		const leftLines = [
@@ -87,7 +102,7 @@ export class WelcomeComponent implements Component {
 
 		// Right column separator
 		const separatorWidth = Math.max(0, rightCol - 2); // padding on each side
-		const separator = ` ${theme.fg("dim", theme.boxRound.horizontal.repeat(separatorWidth))}`;
+		const separator = ` ${this.#hotPink(theme.boxRound.horizontal.repeat(separatorWidth))}`;
 
 		// Recent sessions content
 		const sessionLines: string[] = [];
@@ -135,28 +150,27 @@ export class WelcomeComponent implements Component {
 			"",
 		];
 
-		// Border characters (dim)
+		// Border characters (ANSI hot pink)
 		const hChar = theme.boxRound.horizontal;
-		const h = theme.fg("dim", hChar);
-		const v = theme.fg("dim", theme.boxRound.vertical);
-		const tl = theme.fg("dim", theme.boxRound.topLeft);
-		const tr = theme.fg("dim", theme.boxRound.topRight);
-		const bl = theme.fg("dim", theme.boxRound.bottomLeft);
-		const br = theme.fg("dim", theme.boxRound.bottomRight);
+		const v = this.#hotPink(theme.boxRound.vertical);
+		const tl = this.#hotPink(theme.boxRound.topLeft);
+		const tr = this.#hotPink(theme.boxRound.topRight);
+		const bl = this.#hotPink(theme.boxRound.bottomLeft);
+		const br = this.#hotPink(theme.boxRound.bottomRight);
 
 		const lines: string[] = [];
 
 		// Top border with embedded title
 		const title = ` ${APP_NAME} v${this.version} `;
 		const titlePrefixRaw = hChar.repeat(3);
-		const titleStyled = theme.fg("dim", titlePrefixRaw) + theme.fg("muted", title);
+		const titleStyled = this.#hotPink(titlePrefixRaw) + theme.fg("muted", title);
 		const titleVisLen = visibleWidth(titlePrefixRaw) + visibleWidth(title);
 		const titleSpace = boxWidth - 2;
 		if (titleVisLen >= titleSpace) {
 			lines.push(tl + truncateToWidth(titleStyled, titleSpace) + tr);
 		} else {
 			const afterTitle = titleSpace - titleVisLen;
-			lines.push(tl + titleStyled + theme.fg("dim", hChar.repeat(afterTitle)) + tr);
+			lines.push(tl + titleStyled + this.#hotPink(hChar.repeat(afterTitle)) + tr);
 		}
 
 		// Content rows
@@ -172,9 +186,15 @@ export class WelcomeComponent implements Component {
 		}
 		// Bottom border
 		if (showRightColumn) {
-			lines.push(bl + h.repeat(leftCol) + theme.fg("dim", theme.boxSharp.teeUp) + h.repeat(rightCol) + br);
+			lines.push(
+				bl +
+					this.#hotPink(hChar.repeat(leftCol)) +
+					this.#hotPink(theme.boxSharp.teeUp) +
+					this.#hotPink(hChar.repeat(rightCol)) +
+					br,
+			);
 		} else {
-			lines.push(bl + h.repeat(leftCol) + br);
+			lines.push(bl + this.#hotPink(hChar.repeat(leftCol)) + br);
 		}
 
 		return lines;
@@ -191,34 +211,63 @@ export class WelcomeComponent implements Component {
 		return padding(leftPad) + text + padding(rightPad);
 	}
 
-	/** Apply magenta→cyan gradient to a string */
-	#gradientLine(line: string): string {
-		const colors = [
-			"\x1b[38;5;199m", // bright magenta
-			"\x1b[38;5;171m", // magenta-purple
-			"\x1b[38;5;135m", // purple
-			"\x1b[38;5;99m", // purple-blue
-			"\x1b[38;5;75m", // cyan-blue
-			"\x1b[38;5;51m", // bright cyan
+	/** Apply a radial gradient (cyan at center → magenta at edges) to the logo */
+	#radialGradient(logo: string[], centerCol: number, centerRow: number): string[] {
+		const cx = centerCol - 1;
+		const cy = centerRow - 1;
+
+		let maxDist = 0;
+		for (let row = 0; row < logo.length; row++) {
+			for (let col = 0; col < logo[row].length; col++) {
+				if (logo[row][col] !== " ") {
+					const dist = Math.sqrt((col - cx) ** 2 + (row - cy) ** 2);
+					if (dist > maxDist) maxDist = dist;
+				}
+			}
+		}
+
+		const stops = [
+			[0, 255, 255],
+			[75, 200, 255],
+			[122, 122, 230],
+			[154, 90, 230],
+			[179, 45, 198],
+			[45, 20, 80],
 		];
 		const reset = "\x1b[0m";
 
-		let result = "";
-		let colorIdx = 0;
-		const step = Math.max(1, Math.floor(line.length / colors.length));
+		return logo.map((line, row) => {
+			let result = "";
+			for (let col = 0; col < line.length; col++) {
+				const char = line[col];
+				const isAfterCenter = col === cx + 1 && row === cy && char === " ";
+				if (char === " " && !isAfterCenter) {
+					result += char;
+					continue;
+				}
+				if (isAfterCenter) {
+					result += `\x1b[48;2;40;112;140m ${reset}\x1b[49m`;
+					continue;
+				}
+				const dist = Math.sqrt((col - cx) ** 2 + (row - cy) ** 2);
+				const t = maxDist > 0 ? Math.min(1, dist / maxDist) : 0;
+				const scaledT = t * (stops.length - 1);
+				const idx = Math.min(Math.floor(scaledT), stops.length - 2);
+				const frac = scaledT - idx;
+				const r = Math.round(stops[idx][0] + (stops[idx + 1][0] - stops[idx][0]) * frac);
+				const g = Math.round(stops[idx][1] + (stops[idx + 1][1] - stops[idx][1]) * frac);
+				const b = Math.round(stops[idx][2] + (stops[idx + 1][2] - stops[idx][2]) * frac);
+				const isCenter = col === cx && row === cy;
+				const bg = isCenter ? "\x1b[48;2;40;112;140m" : "";
+				const bgReset = isCenter ? "\x1b[49m" : "";
+				result += `${bg}\x1b[38;2;${r};${g};${b}m${char}${reset}${bgReset}`;
+			}
+			return result;
+		});
+	}
 
-		for (let i = 0; i < line.length; i++) {
-			if (i > 0 && i % step === 0 && colorIdx < colors.length - 1) {
-				colorIdx++;
-			}
-			const char = line[i];
-			if (char !== " ") {
-				result += colors[colorIdx] + char + reset;
-			} else {
-				result += char;
-			}
-		}
-		return result;
+	#hotPink(text: string): string {
+		return `${WELCOME_BORDER_HOT_PINK_ANSI}${text}${ANSI_FOREGROUND_RESET}`;
 	}
 
 	/** Fit string to exact width with ANSI-aware truncation/padding */

shyndman/danger-pi

packages/coding-agent/src/modes/components/welcome.ts
@@ -124,6 +124,9 @@ export function renderWelcomeTip(tip: string, boxWidth: number, phase = 0): stri
 	return lines;
 }
 
+const WELCOME_BORDER_HOT_PINK_ANSI = "\x1b[38;2;247;18;232m";
+const ANSI_FOREGROUND_RESET = "\x1b[39m";
+
 export interface RecentSession {
 	name: string;
 	timeAgo: string;
@@ -258,8 +261,24 @@ export class WelcomeComponent implements Component {
 		const leftCol = showRightColumn ? dualLeftCol : boxWidth - 2;
 		const rightCol = showRightColumn ? dualRightCol : 0;
 
-		// Logo: pick a frame from the intro animation if active, else the resting frame.
-		const logoColored = this.#currentLogoFrame();
+		// Block-based OMP logo (gradient: magenta → cyan)
+		// biome-ignore format: preserve ASCII art layout
+		const PI_LARGE_LOGO = [
+      "     #▓░▓##       ",
+      "   #░░░░░░░▓#     ",
+      "  ##░░░░░░░▓░#    ",
+      "  #█▒██\ue22c ██▓█#    ",
+      "  #█ X ██ X █#    ",
+      "  #█░░█  ░░██#    ",
+      "░░  (██░███)  /░  ",
+      "░▒*._█ █  █  /░░/ ",
+      "    \\#// /       ",
+      "░░░▒** - - *░░░░\\ ",
+      "░░           ░▓  ",
+    ];
+
+		// Apply gradient to logo
+		const logoColored = this.#radialGradient(PI_LARGE_LOGO, 8, 4);
 
 		// Left column - centered content
 		const leftLines = [
@@ -274,7 +293,7 @@ export class WelcomeComponent implements Component {
 
 		// Right column separator
 		const separatorWidth = Math.max(0, rightCol - 2); // padding on each side
-		const separator = ` ${theme.fg("dim", theme.boxRound.horizontal.repeat(separatorWidth))}`;
+		const separator = ` ${this.#hotPink(theme.boxRound.horizontal.repeat(separatorWidth))}`;
 
 		// Recent sessions content
 		const sessionLines: string[] = [];
@@ -341,28 +360,27 @@ export class WelcomeComponent implements Component {
 			"",
 		];
 
-		// Border characters (dim)
+		// Border characters (ANSI hot pink)
 		const hChar = theme.boxRound.horizontal;
-		const h = theme.fg("dim", hChar);
-		const v = theme.fg("dim", theme.boxRound.vertical);
-		const tl = theme.fg("dim", theme.boxRound.topLeft);
-		const tr = theme.fg("dim", theme.boxRound.topRight);
-		const bl = theme.fg("dim", theme.boxRound.bottomLeft);
-		const br = theme.fg("dim", theme.boxRound.bottomRight);
+		const v = this.#hotPink(theme.boxRound.vertical);
+		const tl = this.#hotPink(theme.boxRound.topLeft);
+		const tr = this.#hotPink(theme.boxRound.topRight);
+		const bl = this.#hotPink(theme.boxRound.bottomLeft);
+		const br = this.#hotPink(theme.boxRound.bottomRight);
 
 		const lines: string[] = [];
 
 		// Top border with embedded title
 		const title = ` ${APP_NAME} v${this.version} `;
 		const titlePrefixRaw = hChar.repeat(3);
-		const titleStyled = theme.fg("dim", titlePrefixRaw) + theme.fg("muted", title);
+		const titleStyled = this.#hotPink(titlePrefixRaw) + theme.fg("muted", title);
 		const titleVisLen = visibleWidth(titlePrefixRaw) + visibleWidth(title);
 		const titleSpace = boxWidth - 2;
 		if (titleVisLen >= titleSpace) {
 			lines.push(tl + truncateToWidth(titleStyled, titleSpace) + tr);
 		} else {
 			const afterTitle = titleSpace - titleVisLen;
-			lines.push(tl + titleStyled + theme.fg("dim", hChar.repeat(afterTitle)) + tr);
+			lines.push(tl + titleStyled + this.#hotPink(hChar.repeat(afterTitle)) + tr);
 		}
 
 		// Content rows
@@ -378,9 +396,15 @@ export class WelcomeComponent implements Component {
 		}
 		// Bottom border
 		if (showRightColumn) {
-			lines.push(bl + h.repeat(leftCol) + theme.fg("dim", theme.boxRound.teeUp) + h.repeat(rightCol) + br);
+			lines.push(
+				bl +
+					this.#hotPink(hChar.repeat(leftCol)) +
+					this.#hotPink(theme.boxSharp.teeUp) +
+					this.#hotPink(hChar.repeat(rightCol)) +
+					br,
+			);
 		} else {
-			lines.push(bl + h.repeat(leftCol) + br);
+			lines.push(bl + this.#hotPink(hChar.repeat(leftCol)) + br);
 		}
 
 		// Randomly picked tip, rendered directly beneath the box.
@@ -416,6 +440,64 @@ export class WelcomeComponent implements Component {
 		return padding(leftPad) + text + padding(rightPad);
 	}
 
+	/** Apply a radial gradient (cyan at center → magenta at edges) to the logo */
+	#radialGradient(logo: string[], centerCol: number, centerRow: number): string[] {
+		const cx = centerCol - 1;
+		const cy = centerRow - 1;
+
+		let maxDist = 0;
+		for (let row = 0; row < logo.length; row++) {
+			for (let col = 0; col < logo[row].length; col++) {
+				if (logo[row][col] !== " ") {
+					const dist = Math.sqrt((col - cx) ** 2 + (row - cy) ** 2);
+					if (dist > maxDist) maxDist = dist;
+				}
+			}
+		}
+
+		const stops = [
+			[0, 255, 255],
+			[75, 200, 255],
+			[122, 122, 230],
+			[154, 90, 230],
+			[179, 45, 198],
+			[45, 20, 80],
+		];
+		const reset = "\x1b[0m";
+
+		return logo.map((line, row) => {
+			let result = "";
+			for (let col = 0; col < line.length; col++) {
+				const char = line[col];
+				const isAfterCenter = col === cx + 1 && row === cy && char === " ";
+				if (char === " " && !isAfterCenter) {
+					result += char;
+					continue;
+				}
+				if (isAfterCenter) {
+					result += `\x1b[48;2;40;112;140m ${reset}\x1b[49m`;
+					continue;
+				}
+				const dist = Math.sqrt((col - cx) ** 2 + (row - cy) ** 2);
+				const t = maxDist > 0 ? Math.min(1, dist / maxDist) : 0;
+				const scaledT = t * (stops.length - 1);
+				const idx = Math.min(Math.floor(scaledT), stops.length - 2);
+				const frac = scaledT - idx;
+				const r = Math.round(stops[idx][0] + (stops[idx + 1][0] - stops[idx][0]) * frac);
+				const g = Math.round(stops[idx][1] + (stops[idx + 1][1] - stops[idx][1]) * frac);
+				const b = Math.round(stops[idx][2] + (stops[idx + 1][2] - stops[idx][2]) * frac);
+				const isCenter = col === cx && row === cy;
+				const bg = isCenter ? "\x1b[48;2;40;112;140m" : "";
+				const bgReset = isCenter ? "\x1b[49m" : "";
+				result += `${bg}\x1b[38;2;${r};${g};${b}m${char}${reset}${bgReset}`;
+			}
+			return result;
+		});
+	}
+
+	#hotPink(text: string): string {
+		return `${WELCOME_BORDER_HOT_PINK_ANSI}${text}${ANSI_FOREGROUND_RESET}`;
+	}
 	/** Fit string to exact width with ANSI-aware truncation/padding */
 	#fitToWidth(str: string, width: number): string {
 		const visLen = visibleWidth(str);
@@ -440,17 +522,17 @@ export class WelcomeComponent implements Component {
 		}
 		return str + padding(width - visLen);
 	}
-
-	/** Pick the logo frame for the current intro phase, or the resting frame. */
-	#currentLogoFrame(): readonly string[] {
-		if (this.#animStart == null) return REST_FRAME;
-		const elapsed = performance.now() - this.#animStart;
-		if (elapsed >= INTRO_MS) return REST_FRAME;
-		return introLogoFrame(elapsed / INTRO_MS);
-	}
 }
 
-export const PI_LOGO = ["▀██████████▀", " ╘██    ██  ", "  ██    ██  ", "  ██    ██  ", " ▄██▄  ▄██▄ "];
+// biome-ignore format: preserve ASCII art layout
+export const PI_LOGO = [
+  "     ～～～～～    ",
+  "  ╭             ╮",
+  "  |             |",
+  "      ◎     ◎    ",
+  "    ¬    △    ⌐  ",
+  "       ↕↕↕↕↕      ",
+];
 
 /** Multi-stop palette for the diagonal gradient. */
 const GRADIENT_STOPS: ReadonlyArray<readonly [number, number, number]> = [
@@ -551,28 +633,3 @@ export function gradientLogo(lines: readonly string[], phase = 0, shine?: ShineC
 const INTRO_MS = 3000;
 /** Render cadence during the intro (~30fps). */
 const INTRO_TICK_MS = 33;
-/** Number of full gradient rotations the sweep performs before settling. */
-const INTRO_SWEEPS = 2.5;
-/** Number of times the shine highlight crosses the diagonal across the intro. */
-const INTRO_SHINE_TRAVERSALS = 3;
-
-/**
- * Logo frame for a normalized intro progress in [0, 1).
- *
- * Ease-out cubic so the spin decelerates into the resting state. The gradient
- * sweeps backward through INTRO_SWEEPS full rotations (`eased == 1` → phase =
- * 0 = resting frame) while the shine traverses the diagonal at a steady pace,
- * decoupled from the gradient phase so the two layers parallax; its strength
- * fades with the same ease-out curve so the highlight is gone by the resting
- * frame.
- */
-function introLogoFrame(progress: number): string[] {
-	const eased = 1 - (1 - progress) ** 3;
-	const phase = ((((1 - eased) * INTRO_SWEEPS) % 1) + 1) % 1;
-	const shinePos = (((progress * INTRO_SHINE_TRAVERSALS) % 1) + 1) % 1;
-	const shineStrength = (1 - eased) ** 1.5;
-	return gradientLogo(PI_LOGO, phase, { strength: shineStrength, pos: shinePos });
-}
-
-/** Resting gradient frame, cached for re-renders outside of the intro. */
-const REST_FRAME = gradientLogo(PI_LOGO, 0);